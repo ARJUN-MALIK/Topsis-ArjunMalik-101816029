2bf4119c11d17d5d4f7ddfaccba0285dd84ec858
arjun3011 <47695713+ARJUN-MALIK@users.noreply.github.com>
2020-11-13 22:20:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'Topsis-ArjunMalik-101816029',         # How you named your package folder (MyLib)
-  packages = ['Topsis-ArjunMalik-101816029'],   # Chose the same as "name"
+  name = 'Topsis_ArjunMalik_101816029',         # How you named your package folder (MyLib)
+  packages = ['Topsis_ArjunMalik_101816029'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This library helps in implementing topsis method',   # Give a short description about your library
